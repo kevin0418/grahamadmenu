89e25a332ed489c91b0edcb26581868b03075757
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2024-09-16 22:19:15 +1000

    add ad
    add ad

---
+++ grahammenu.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.write("## Sport Building Sale in :red[Carseldine] ")
+
+
+st.markdown(
+"""
+    
+    :blue[Prime Investment Opportunity] 
+    Welcome to an exceptional investment opportunity in the heart of Carseldine
+
+    :blue[Loyalty Long-term Tenant]
+    Health Club since 2005, with a strong brand presence in the fitness industry
+
+    :blue[Secure Income Stream]
+    ensures a stable and growing income stream
+
+    :blue[Future Development]
+    Queensland Government Carseldine Urban Village project
+  
+
+    - [Infornation Memorandom IM ](https://grahampdf3-xguyyke5hv8nvwhq3pxbwx.streamlit.app/)
+        
+    
+    - [Watch to video ](https://youtu.be/z31uqT19x5s)
+
+    
+    - [Brochure ](https://kevin-grahambrochure1.streamlit.app/)
+"""
+)     
+
+st.markdown(
+"""    
+       Contact information  
+       Yong Real Estate
+  
+     Sue Yi : 0412 317 386
+     Kevin OH : 0402 283 003
+    
+
+"""
+)  
